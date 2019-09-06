d790a1f4344b6d7f675322fca670db6cdc9ac5b1
Gavin Dreyer <gavindreyer@Gavins-MacBook-Pro.local>
2019-09-06 09:21:30 -0700

    Finished cards/index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container')
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+
+.then(function (response) {
+    // handle success
+    console.log(response)
+    response.data.articles.javascript.forEach(item => {
+        cardsContainer.append(cardCreator(item))
+    })
+    
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+
+
+
+function cardCreator(object) {
+    const cardDiv = document.createElement('div');
+    const headLine = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authSpan = document.createElement('span');
+
+    //structure
+    cardDiv.append(headLine);
+    cardDiv.append(author);
+    author.append(imgContainer);
+    imgContainer.append(image);
+    author.append(authSpan);
+
+    //classes
+    cardDiv.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+
+    //content
+    headLine.textContent = object.headline;
+    image.setAttribute = ('src', object.authorPhoto);
+    authSpan.textContent = `By ${object.authorName}`;
+
+    return cardDiv
+}
+
